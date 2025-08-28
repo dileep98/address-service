b9451b61d7f295c0f6315210f3b8e6e40148007c
dileep98 <dk26092@gmail.com>
2025-08-27 22:19:18 -0400

    address controller get mapping

---
+++ src/main/java/com/dk/addressservice/controller/AddressController.java
@@ -0,0 +1,13 @@
+package com.dk.addressservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressController {
+
+    @GetMapping("/address")
+    public String getAddress(){
+        return "Delray Beach, Florida";
+    }
+}
